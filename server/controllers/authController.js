1dcb252500230f5f139368ffbbc5bc5b39c9b0be
Enjebel <enjebeln@gmail.com>
2025-12-27 01:48:54 +0100

    chore: organized folder structure with middleware, utils, and auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+const crypto = require('crypto');
+const sendEmail = require('../utils/sendEmail');
+
+exports.registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+        const user = await User.create({ username, email, password });
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user && (await user.comparePassword(password))) {
+            res.json({
+                _id: user._id,
+                username: user.username,
+                token: generateToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.forgotPassword = async (req, res) => {
+    // ... logic we wrote in previous step ...
+};
